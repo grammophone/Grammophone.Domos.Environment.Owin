e46d26e74ef342f01ee2099e89e702ed40bbc5e4
grammophone <emmanuel.liossis@gmail.com>
2016-09-09 15:42:10 +0300

    Add NuGet reference and fix namespaces.

--- OwinUserContext.cs
+++ OwinUserContext.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Grammophone.Domos.Environmnent;
+using Microsoft.AspNet.Identity;
 
 namespace Grammophone.Domos.Environment.Owin
 {
